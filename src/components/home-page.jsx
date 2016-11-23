b2b13afcdd2e53c0a846c4e518df6e38b375c0fa
Pratibha <pratibhamansinghka@gmail.com>
2016-11-23 16:31:49 +0530

    extract loader component

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import FilterBarConnector from '../containers/filter-bar-connector';
 import ProductList from './product-list';
-import loader from '../images/loader.svg';
+import Loader from './loader';
 
 class HomePage extends Component {
   componentDidMount() {
@@ -12,10 +12,7 @@ class HomePage extends Component {
   render() {
     return (
       <div className="home-page row">
-
-        <div className={this.props.displayLoader ? 'show' : 'hide'}>
-          <img className='loader' src={loader} />
-        </div>
+        <Loader displayLoader={this.props.displayLoader} />
         <div className="col-md-3">
           <FilterBarConnector />
         </div>
