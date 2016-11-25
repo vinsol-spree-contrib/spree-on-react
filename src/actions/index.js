ab7fd867f061b94cb0dbc3e3188297dcb7c0220f
Akshat Singhal <akshat@vinsol.com>
2016-11-25 13:36:44 +0530

    Refactor connectors and actions to follow redux action syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,10 @@
-import productList from './product-list';
 import products from './products';
 import taxonomies from './taxonomies';
 import loader from './loader';
 
 export default {
-  fetchProducts: productList.fetchProducts,
   addProducts: products.addProducts,
-  fetchProduct: products.fetchProduct,
   addProduct: products.addProduct,
-  fetchTaxonomies: taxonomies.fetchTaxonomies,
   addTaxonomies: taxonomies.addTaxonomies,
   displayLoader: loader.displayLoader,
   hideLoader: loader.hideLoader
