37ad24b8d872cb821d2baf716619fe71c499d4f5
Pratibha <pratibhamansinghka@gmail.com>
2016-11-29 16:13:19 +0530

    get categorized list on refreshing

--- src/containers/home-page-connector.js
+++ src/containers/home-page-connector.js
@@ -16,31 +16,44 @@ const mapDispatchToProps = (dispatch) => {
   return {
     triggerInitialSetup: () => {
       dispatch (Actions.displayLoader());
-      let fetchProductsPromise = null;
-      let taxon_link = location.pathname.match(/^\/t(.+)/)
-      if(taxon_link){
-        fetchProductsPromise = ProductsAPI.getCategorizedList(7).then((response) => {
-          let fetchedProducts = JSON.parse(response.text).products;
-
-          dispatch (Actions.addProducts(fetchedProducts));
-        });
-      } else {
-        fetchProductsPromise = ProductsAPI.getList().then((response) => {
-          let fetchedProducts = JSON.parse(response.text).products;
-
-          dispatch (Actions.addProducts(fetchedProducts));
-        });
-      }
-
-      const fetchTaxonomiesPromise = TaxonomyAPI.getList().then((response) => {
-        let fetchedTaxonomies = JSON.parse(response.text).taxonomies;
-
+      let fetchedTaxonomies = null;
+      let taxon_id = null;
+      let taxon_link = location.pathname.match(/^\/t\/(.+)/)
+      TaxonomyAPI.getList().then((response) => {
+        fetchedTaxonomies = JSON.parse(response.text).taxonomies;
         dispatch (Actions.addTaxonomies(fetchedTaxonomies));
+        return fetchedTaxonomies
+      }).then((response) => {
+        if(taxon_link){
+          taxon_id = () => {
+            for (var i = 0; i < response.length; i++) {
+              if(response[i].root.permalink === taxon_link[1]){
+                return response[i].root.id;
+              } else {
+                for (var j = 0; j < response[i].root.taxons.length; j++) {
+                  if(response[i].root.taxons[j].permalink === taxon_link[1]){
+                    return response[i].root.taxons[j].id;
+                  }
+                };
+              }
+            };
+          }
+          ProductsAPI.getCategorizedList(taxon_id()).then((response) => {
+            let fetchedProducts = JSON.parse(response.text).products;
+
+            dispatch (Actions.addProducts(fetchedProducts));
+            dispatch (Actions.hideLoader())
+          });
+        } else {
+          ProductsAPI.getList().then((response) => {
+            let fetchedProducts = JSON.parse(response.text).products;
+
+            dispatch (Actions.addProducts(fetchedProducts));
+            dispatch (Actions.hideLoader())
+          });
+        }
       });
 
-      Promise.all([fetchProductsPromise, fetchTaxonomiesPromise]).then((response) => {
-        dispatch (Actions.hideLoader())
-      });
     }
   };
 };
