9a5c3d9718103739dc434a823ff031d58cf20236
Shubham Gupta <shubham@vinsol.com>
2016-12-19 16:36:13 +0530

    Load orders for the currently logged in user.

--- src/containers/header-connector.js
+++ src/containers/header-connector.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { push } from 'react-redux';
+import { push } from 'react-router-redux';
 
 import Header from '../components/header';
 import Actions from '../actions';
