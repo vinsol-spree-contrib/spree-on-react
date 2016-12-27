5585a5de4643350b5d8be283c69845a3c731de24
Shubham Gupta <shubham@vinsol.com>
2016-12-27 19:43:14 +0530

    Wipe out the order after sign in.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,13 @@
 import APP_ACTIONS from '../constants/app-actions';
 import localStorageAPI from '../services/local-storage-api';
+import orders from './order';
 
 const user = {
   login: (userResponse) => {
     return (dispatch, getState) => {
+      /* Wipe out the order after logging in. */
+      dispatch (orders.clearOrder());
+
       dispatch( {
         type: APP_ACTIONS.LOGIN,
         payload: userResponse
