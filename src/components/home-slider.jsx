78fc319bf0dba80e1b2922eac42295bb19d61a7c
ashwani-sharma <sharma.ashwani007@gmail.com>
2017-06-23 19:28:39 +0530

    homepage slider design added

--- src/components/home-slider.jsx
+++ src/components/home-slider.jsx
@@ -3,25 +3,31 @@ import {Carousel} from 'react-bootstrap'
 import Banner1 from '../images/coffee-images/Banner-1.jpg';
 import Banner2 from '../images/coffee-images/Banner-2.jpeg';
 
+import styles from './styles/components/home-slider.scss';
+
 
 class HomeSlider extends Component {
 
   render () {
     return (
-      <Carousel className="home-top-slider">
-        <Carousel.Item style={{ backgroundImage: `url(${Banner1})` }}>
-          <div className="banner-block">
-            <div className="banner-title">
-              <h3></h3>
-              <p></p>
+      <Carousel className={ " " + styles.homeSlider }>
+        <Carousel.Item className={ " " + styles.homeSliderRow } style={{ backgroundImage: `url(${Banner1})` }}>
+          <div className={ "container " + styles.sliderContainer }>
+            <div className={ "banner-block " + styles.bannerTextContent }>
+              <div className="banner-title">
+                <h3>Banner 1</h3>
+                <p>Lorem ipsum doler set amet. Lorem ipsum doler set amet. Lorem ipsum doler set amet. Lorem ipsum doler set amet.</p>
+              </div>
             </div>
           </div>
         </Carousel.Item>
-        <Carousel.Item style={{ backgroundImage: `url(${Banner2})` }}>
-          <div className="banner-block">
-            <div className="banner-title">
-              <h3></h3>
-              <p></p>
+        <Carousel.Item className={ " " + styles.homeSliderRow } style={{ backgroundImage: `url(${Banner2})` }}>
+          <div className={ "container " + styles.sliderContainer }>
+            <div className={ "banner-block " + styles.bannerTextContent }>
+              <div className="banner-title">
+                <h3>Banner 2</h3>
+                <p>Lorem ipsum doler set amet. Lorem ipsum doler set amet. Lorem ipsum doler set amet. Lorem ipsum doler set amet.</p>
+              </div>
             </div>
           </div>
         </Carousel.Item>
