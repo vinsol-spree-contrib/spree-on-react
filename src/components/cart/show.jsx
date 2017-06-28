1b8fa2aa41299c6698570724bb829ce84fcd6c35
ashwani-sharma <sharma.ashwani007@gmail.com>
2017-06-28 15:44:34 +0530

    cart design added

--- src/components/cart/show.jsx
+++ src/components/cart/show.jsx
@@ -74,33 +74,35 @@ class CartShow extends Component {
 
         return (
           <tr className="line-item" key={`line-item-${idx}`}>
-            <td>
-              <div className="cart-img-block">
-                <Link to={`/products/${ productSlug }`}>
-                  <img  className="product-image img-responsive"
-                        alt='productName'
-                        src={ imageUrl }>
-                  </img>
-                </Link>
-              </div>
+            <td className="cart-item-image-col">
+              <div className="row">
+                <div className="col-sm-2 cart-img-block">
+                  <Link to={`/products/${ productSlug }`} className="cart-image-link">
+                    <img  className="cart-image"
+                          alt='productName'
+                          src={ imageUrl }>
+                    </img>
+                  </Link>
+                </div>
 
-              <div className="cart-img-des hidden-xs">
-                <p><Link to={`/products/${ productSlug }`}>{ lineItem.variant.name }</Link></p>
-                <p>{ lineItem.variant.description }</p>
+                <div className="col-sm-10 cart-item-details hidden-xs">
+                  <h4 className="cart-item-title"><Link to={`/products/${ productSlug }`}>{ lineItem.variant.name }</Link></h4>
+                  <p className="cart-item-description">{ lineItem.variant.description }</p>
+                </div>
               </div>
             </td>
 
-            <td>
+            <td className="cart-item-price-col">
               { lineItem.single_display_amount }
             </td>
 
-            <td className="qty-update-block">
+            <td className="qty-update-block cart-item-qty-col">
               <form onSubmit={this.changeQuantity}>
                 <input type='hidden' value={lineItem.id} name='line_item_id' />
                 <input type='hidden' value={lineItem.variant_id} name='line_item_variant_id' />
-                <input type='number' defaultValue={ lineItem.quantity } name='quantity' className="qty-number"/>
+                <input type='number' defaultValue={ lineItem.quantity } name='quantity' className="qty-number cart-item-qty-input"/>
                 <span className="visible-xxs"></span>
-                <button type='submit' className="btn btn-success">
+                <button type='submit' className="cart-item-save-btn button-primary button-small">
                   <FormattedMessage
                     id="label.buttons.save"
                     defaultMessage="Save"
@@ -109,82 +111,86 @@ class CartShow extends Component {
               </form>
             </td>
 
-            <td className="text-right">
+            <td className="text-right cart-item-total-col">
               { lineItem.display_amount }
             </td>
 
             <td className="text-right">
-              <a className='btn btn-danger btn-sm' onClick={() => this.destroyLineItem(lineItems.length, lineItem)}><span className="glyphicon glyphicon-trash"></span></a>
+              <a className='button-primary button-red' onClick={() => this.destroyLineItem(lineItems.length, lineItem)}><span className="glyphicon glyphicon-trash"></span></a>
             </td>
           </tr>
         );
       });
 
       renderString =
-        <div className="row">
-          <div className="col-md-12">
-            <Table striped hover className="cart-table">
-              <thead>
-                <tr>
-                  <th>
-                    <FormattedMessage
-                      id="com.cart--show.tableHeading.product"
-                      defaultMessage="Product"
-                    />
-                  </th>
-                  <th>
-                    <FormattedMessage
-                      id="com.cart--show.tableHeading.price"
-                      defaultMessage="Price"
-                    />
-                  </th>
-                  <th>
-                    <FormattedMessage
-                      id="com.cart--show.tableHeading.qty"
-                      defaultMessage="Qty"
-                    />
-                  </th>
-                  <th className="text-right">
-                    <FormattedMessage
-                      id="com.cart--show.tableHeading.total"
-                      defaultMessage="Total"
-                    />
-                  </th>
-                  <th className="text-right">
-                    <FormattedMessage
-                      id="com.cart--show.tableHeading.actions"
-                      defaultMessage="Actions"
-                    />
-                  </th>
-                </tr>
-              </thead>
+        <div className="cart-items-section">
+          <table className="cart-table cart-items-table">
+            <thead>
+              <tr>
+                <th>
+                  <FormattedMessage
+                    id="com.cart--show.tableHeading.product"
+                    defaultMessage="Product"
+                  />
+                </th>
+                <th>
+                  <FormattedMessage
+                    id="com.cart--show.tableHeading.price"
+                    defaultMessage="Price"
+                  />
+                </th>
+                <th>
+                  <FormattedMessage
+                    id="com.cart--show.tableHeading.qty"
+                    defaultMessage="Qty"
+                  />
+                </th>
+                <th className="text-right cart-item-total-col">
+                  <FormattedMessage
+                    id="com.cart--show.tableHeading.total"
+                    defaultMessage="Total"
+                  />
+                </th>
+                <th className="text-right">
+                  <FormattedMessage
+                    id="com.cart--show.tableHeading.actions"
+                    defaultMessage="Actions"
+                  />
+                </th>
+              </tr>
+            </thead>
 
-              <tbody>
-                { lineItemList }
-              </tbody>
-            </Table>
+            <tbody>
+              { lineItemList }
+            </tbody>
+          </table>
 
-            <div className="text-right cart-empty-update-row">
-              <a className='link btn btn-empty btn-default pull-left' onClick={this.emptyCart}>
-                <FormattedMessage
-                  id="label.buttons.emptyCart"
-                  defaultMessage="Empty Cart"
-                />
-              </a>
-              <Link className='link btn btn-primary btn-lg' to={ APP_ROUTES.homePageRoute }>
-                <span className="glyphicon glyphicon-chevron-left" />
-                &nbsp;
-                <FormattedMessage
-                  id="com.cart--show.continueShopping"
-                  defaultMessage="Continue Shopping"
-                />
-              </Link>
-              <a className='btn btn-success btn-lg' onClick={this.doCheckout}>
-                <FormattedMessage
-                  id="label.buttons.placeOrder"
-                  defaultMessage="Place Order"
-                />
-              </a>
+          <div className="cart-buttons-row">
+            <div className="row">
+              <div className="col-sm-8">
+                <a className='button-primary button-green' onClick={this.emptyCart}>
+                  <FormattedMessage
+                    id="label.buttons.emptyCart"
+                    defaultMessage="Empty Cart"
+                  />
+                </a>
+                <Link className='button-primary button-white' to={ APP_ROUTES.homePageRoute }>
+                  <span className="glyphicon glyphicon-chevron-left" />
+                  &nbsp;
+                  <FormattedMessage
+                    id="com.cart--show.continueShopping"
+                    defaultMessage="Continue Shopping"
+                  />
+                </Link>
+              </div>
+              <div className="col-sm-4 text-right">
+                <a className='button-primary' onClick={this.doCheckout}>
+                  <FormattedMessage
+                    id="label.buttons.placeOrder"
+                    defaultMessage="Place Order"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>;
@@ -192,19 +198,23 @@ class CartShow extends Component {
 
     return (
       <Layout>
-        <div className="big-box-heading secondary spacing">
-          <FormattedMessage
-            id="com.cart--show.header"
-            defaultMessage="Shopping Cart"
-          />
-          <small className="show">
-            <FormattedMessage
-              id="com.cart--show.helpMessage"
-              defaultMessage="NEED HELP? Call xxxx-xxxx-xxxx"
-            />
-          </small>
+        <div className="cart-section">
+          <div className="container">
+            <div className="section-heading">
+              <FormattedMessage
+                id="com.cart--show.header"
+                defaultMessage="Shopping Cart"
+              />
+              <small className="cart-contact-block">
+                <FormattedMessage
+                  id="com.cart--show.helpMessage"
+                  defaultMessage="NEED HELP? Call xxxx-xxxx-xxxx"
+                />
+              </small>
+            </div>
+            {renderString}
+          </div>
         </div>
-        {renderString}
       </Layout>
     );
   };
